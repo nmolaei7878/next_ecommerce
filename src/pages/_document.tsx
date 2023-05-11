de64823d8bc556d66e6fb7b2e5f26cc7b9ecd5d1
mohammad <mohammad@mohammads-MacBook-Pro.local>
2023-05-12 02:34:15 +0330

    debug icons path

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.jpg"></link>
+        <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
         <meta name="theme-color" content="#fff" />
         <meta
           name="viewport"
