178e58e3d4c5ab4a21cb43ae42413f3ee19a26d4
mohammd molaei <mohammadmolaei7800@gmail.com>
2023-12-09 00:20:12 +0330

    offline

---
+++ src/pages/_offline.jsx
@@ -0,0 +1,9 @@
+const OfflinePage = () => {
+  return (
+    <div className="bg-slate-800 flex items-center justify-center h-screen w-screen ">
+      You Are Offline Now Come Back Later
+    </div>
+  );
+};
+
+export default OfflinePage;
